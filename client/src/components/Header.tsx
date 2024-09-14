d390d3a0d62782590428237a1997381a77d725fc
Eugene Letunov <defrazo@inbox.ru>
2024-09-14 23:01:01 +0500

    Second commit

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,12 +5,12 @@ import styles from './Header.module.scss';
 const Header: React.FC = () => {
     return (
         <header className={styles["header-container"]}>
-            <a href="/apricode/#">
+            <a href="/moneybank/#">
                 <img src={Logo} alt="Логотип" className={styles["logo__image"]} />
                 <span className={styles["logo__text"]}>MoneyBank</span>
             </a>
             <nav className={styles["nav-links"]}>
-                <a href="/apricode/#">Главная</a>
+                <a href="/moneybank/#">Главная</a>
             </nav>
         </header>
     );
